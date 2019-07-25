8e310c2d107501a53180d55d18cb9b59ba361cab
leah0913 <46901930+leah0913@users.noreply.github.com>
2019-07-25 00:13:03 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,28 @@
+import { Songs } from '../../app/songInfo';
+
+export const MYSONGS: Songs[] = [
+    { songName: 'Knees', 
+      artist: 'IU',
+      genre: 'Ballad', 
+      yrReleased:'2015',
+      picture:'IU.jpeg'},
+
+      { songName: 'The One Who I Wish To Be', 
+      artist: 'Seung-Hwan Lee',
+      genre: 'Ballad', 
+      yrReleased:'1995',
+      picture:'SeungHwan.jpeg'},
+      
+      { songName: 'Lie Lie Lie',
+      artist: 'Juck Lee', 
+      genre: 'Ballad', 
+      yrReleased:'2013',
+      picture:'Juck.jpeg'},
+      
+      { songName: "Don't Cry", 
+      artist: 'Hyun-Woo Ha',
+      genre: 'Rock Ballad', 
+      yrReleased:'2016',
+      picture:'HyunWoo.jpeg'}
+]
+
